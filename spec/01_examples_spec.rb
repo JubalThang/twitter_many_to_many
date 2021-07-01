16ffdea55af9c560e09f8e0d35ad4ca5479342d0
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-07-01 12:17:47 -0700

    update specs

--- spec/01_examples_spec.rb
+++ spec/01_examples_spec.rb
@@ -1,5 +1,5 @@
 # We can build more complicated relationships using one to many more than once. 
-RSpec.describe "Aquarium has many fish through exhibits" do 
+RSpec.describe "Aquarium domain" do 
   let(:aquarium) { Aquarium.new(name: "Aquarium of the Pacific")}
   let(:coral_reefs_exhibit) { Exhibit.new(name: "Coral Reefs: Nature's Underwater Cities", aquarium: aquarium)}
   let(:shark_lagoon_exhibit) { Exhibit.new(name: "Shark Lagoon", aquarium: aquarium)}
@@ -9,79 +9,92 @@ RSpec.describe "Aquarium has many fish through exhibits" do
   let(:zebra_shark) { Fish.new(name: "Zebra Shark", exhibit: shark_lagoon_exhibit)}
   let(:grey_reef_shark) {Fish.new(name: "Grey Reef Shark",exhibit: shark_lagoon_exhibit)}
   let(:epaulette_shark){ Fish.new(name: "Epaulette Shark", exhibit: shark_lagoon_exhibit)}
-  describe "Aquarium" do 
-    describe ".all" do 
-      it "returns an array of all aquariums" do 
-        aquarium
-        expect(Aquarium.all).to include(aquarium)
+
+  describe "Basics" do 
+    describe "Aquarium" do 
+      describe ".all" do 
+        it "returns an array of all aquariums" do 
+          expect(Aquarium.all).to eq(Aquarium.class_variable_get("@@all"))
+        end
+      end
+
+      describe "#initialize" do 
+        it "accepts a hash of attributes as an argument, including a name, and saves the aquarium to @@all" do 
+          expect { aquarium }.not_to raise_error
+          expect(aquarium.instance_variable_get("@name")).to eq("Aquarium of the Pacific")
+          expect(Aquarium.class_variable_get("@@all")).to include(aquarium)
+        end
       end
     end
-    describe "#initialize" do 
-      it "accepts a hash of attributes as an argument, including a name, and saves the aquarium to @@all" do 
-        expect { aquarium }.not_to raise_error
-        expect(aquarium.instance_variable_get("@name")).to eq("Aquarium of the Pacific")
-        expect(Aquarium.class_variable_get("@@all")).to include(aquarium)
+    
+    describe "Exhibit" do 
+      describe ".all" do 
+        it ".all returns an array of @@all the Exhibits we've created so far" do 
+          expect(Exhibit.all).to eq(Exhibit.class_variable_get("@@all"))
+        end
+      end
+      
+      describe "#initialize" do 
+        it "accepts a hash of attributes as an argument, including a name and an aquarium, and saves the exhibit to @@all" do 
+          expect { coral_reefs_exhibit }.not_to raise_error
+          expect(coral_reefs_exhibit.instance_variable_get("@name")).to eq("Coral Reefs: Nature's Underwater Cities")
+          expect(coral_reefs_exhibit.instance_variable_get("@aquarium")).to eq(aquarium)
+          expect(Exhibit.class_variable_get("@@all")).to include(coral_reefs_exhibit)
+        end
       end
     end
-    describe "#exhibits" do 
-      it "(has many exhibits) returns an array of Exhibit objects" do 
-        [aquarium, coral_reefs_exhibit, shark_lagoon_exhibit]
-        expect(aquarium.exhibits).to include(coral_reefs_exhibit)
-        expect(aquarium.exhibits).to include(shark_lagoon_exhibit)
+
+    describe "Fish" do 
+      describe ".all" do 
+        it ".all returns an array of @@all the Fish we've created so far" do 
+          expect(Fish.all).to eq(Fish.class_variable_get("@@all"))
+        end
+      end
+      
+      describe "#initialize" do
+        it "accepts a hash of attributes as an argument, including a name and an exhibit, and saves the fish to @@all" do 
+          expect { grey_reef_shark }.not_to raise_error
+          expect(grey_reef_shark.instance_variable_get("@name")).to eq("Grey Reef Shark")
+          expect(grey_reef_shark.instance_variable_get("@exhibit")).to eq(shark_lagoon_exhibit)
+          expect(Fish.class_variable_get("@@all")).to include(grey_reef_shark)
+        end
       end
     end
   end
 
-  describe "Exhibit" do 
-    describe ".all" do 
-      it ".all returns an array of @@all the Exhibits we've created so far" do 
-        [coral_reefs_exhibit, shark_lagoon_exhibit]
-        expect(Exhibit.all).to include(coral_reefs_exhibit, shark_lagoon_exhibit)
+  describe "Associations" do 
+
+    describe "Exhibit" do
+      describe "#fish" do 
+        it "(has many fish) returns an array of all the fish in this exhibit" do 
+          [shark_lagoon_exhibit, zebra_shark, grey_reef_shark, epaulette_shark]
+          expect(shark_lagoon_exhibit.fish).to include(zebra_shark, grey_reef_shark, epaulette_shark)
+        end
       end
     end
 
-    describe "#initialize" do 
-      it "accepts a hash of attributes as an argument, including a name and an aquarium, and saves the exhibit to @@all" do 
-        expect { coral_reefs_exhibit }.not_to raise_error
-        expect(Exhibit.class_variable_get("@@all")).to include(coral_reefs_exhibit)
+    describe "Aquarium" do 
+      describe "#exhibits" do 
+        it "(has many exhibits) returns an array of Exhibit objects" do 
+          [aquarium, coral_reefs_exhibit, shark_lagoon_exhibit]
+          expect(aquarium.exhibits).to include(coral_reefs_exhibit)
+          expect(aquarium.exhibits).to include(shark_lagoon_exhibit)
+        end
       end
-    end
 
-    describe "#fish" do 
-      it "returns an array of all the fish in this exhibit" do 
-        [shark_lagoon_exhibit, zebra_shark, grey_reef_shark, epaulette_shark]
-        expect(shark_lagoon_exhibit.fish).to include(zebra_shark, grey_reef_shark, epaulette_shark)
+      describe "#fish" do 
+        it "(has many fish through exhibits) returns an array of Fish objects associated with" do 
+          [aquarium, shark_lagoon_exhibit, grey_reef_shark, epaulette_shark]
+          expect(aquarium.fish).to include(grey_reef_shark)
+          expect(aquarium.fish).to include(epaulette_shark)
+        end
       end
     end
   end
 
-  describe "Fish" do 
-    describe ".all" do 
-      it ".all returns an array of @@all the Fish we've created so far" do 
-        [grey_reef_shark, epaulette_shark]
-        expect(Fish.all).to include(grey_reef_shark, epaulette_shark)
-      end
-    end
-
-    describe "#initialize" do
-      it "accepts a hash of attributes as an argument, including a name and an exhibit, and saves the fish to @@all" do 
-        expect { grey_reef_shark }.not_to raise_error
-        expect(Fish.class_variable_get("@@all")).to include(grey_reef_shark)
-      end
-    end
-  end
-  describe "Aquarium (cont'd)" do
-    describe "#fish" do 
-      it "(has many fish through exhibits) returns an array of Fish objects associated with" do 
-        [aquarium, shark_lagoon_exhibit, grey_reef_shark, epaulette_shark]
-        expect(aquarium.fish).to include(grey_reef_shark)
-        expect(aquarium.fish).to include(epaulette_shark)
-      end
-    end
-  end
 end
 
-RSpec.describe "Many to Many Doctor <=> Patient through appointments" do
+RSpec.describe "Doctor domain" do
   let(:drew) { Doctor.new(name: "Dr. Drew") }
   let(:amy) { Patient.new(name: "Amy Adams") }
   let(:appointment) { Appointment.new(time: "2PM", doctor: drew, patient: amy)}
@@ -89,8 +102,7 @@ RSpec.describe "Many to Many Doctor <=> Patient through appointments" do
     describe "Doctor" do 
       describe ".all" do 
         it "returns an array of all the doctors we have created." do 
-          drew
-          expect(Doctor.all).to include(drew)
+          expect(Doctor.all).to eq(Doctor.class_variable_get("@@all"))
         end
       end
   
@@ -105,8 +117,7 @@ RSpec.describe "Many to Many Doctor <=> Patient through appointments" do
     describe "Patient" do 
       describe ".all" do 
         it "returns an array of all the patients we have created." do 
-          amy
-          expect(Patient.all).to include(amy)
+          expect(Patient.all).to eq(Patient.class_variable_get("@@all"))
         end
       end
   
@@ -117,23 +128,23 @@ RSpec.describe "Many to Many Doctor <=> Patient through appointments" do
         end
       end
     end
+    
+    describe "Appointment" do 
+      describe ".all" do 
+        it "returns an array of all appointments created" do
+          expect(Appointment.all).to eq(Appointment.class_variable_get("@@all"))
+        end
+      end
+  
+      describe "#initialize" do
+        it "takes a hash of attributes as an argument, including a time, doctor and patient, and stores the appointment in @@all" do 
+          expect { appointment }.not_to raise_error
+          expect(Appointment.class_variable_get("@@all")).to include(appointment)
+        end
+      end
+    end
   end
   
-  describe "Appointment" do 
-    describe ".all" do 
-      it "returns an array of all appointments created" do
-        appointment
-        expect(Appointment.all).to include(appointment)
-      end
-    end
-
-    describe "#initialize" do
-      it "takes a hash of attributes as an argument, including a time, doctor and patient, and stores the appointment in @@all" do 
-        expect { appointment }.not_to raise_error
-        expect(Appointment.class_variable_get("@@all")).to include(appointment)
-      end
-    end
-  end
 
   describe "Associations" do 
     describe "Appointment" do 
@@ -182,5 +193,4 @@ RSpec.describe "Many to Many Doctor <=> Patient through appointments" do
       end
     end
   end
-
 end
